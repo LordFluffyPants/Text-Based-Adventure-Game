58c7816c709fcb4f35119c8aed72f83cc5ddc44e
LordFluffyPants <rixmaddi@gmail.com>
2015-01-18 20:11:48 -0500

    added project files

---
+++ Text Based Adventure Game/Text Based Adventure Game/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+
+//system("cls"); This will clear the command prompt
+//system("PAUSE");standard press anykey to continue
+
+int main(){
+	ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
+	system("mode 650");
+	bool loop = true;
+	string input; //Keeping this variable will store the last user input
+
+	while(loop){
+		cout<< "WELCOME TO name pending game" <<endl;
+		system("PAUSE");
+		
+		
+
+
+		
+	}
+	return 0;
+}
